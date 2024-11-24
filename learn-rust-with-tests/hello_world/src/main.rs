a4026356a3b1f12c05a8876d4b072cc885786e34
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-24 14:32:11 -0600

    Add test to hello_world project.

--- learn-rust-with-tests/hello_world/src/main.rs
+++ learn-rust-with-tests/hello_world/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, World!");
+    println!("{}", hello_world());
+}
+
+
+fn hello_world() -> String {
+    return "Hello, World!".to_string();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello_world() {
+        let result = hello_world();
+        assert_eq!(result, "Hello, World!");
+    }
 }
