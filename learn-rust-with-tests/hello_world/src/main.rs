e27899d3638a75008a6092afbbdd1f286bcb520c
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2024-11-24 16:37:10 -0600

    Update hello_word rust project.

--- learn-rust-with-tests/hello_world/src/main.rs
+++ learn-rust-with-tests/hello_world/src/main.rs
@@ -1,11 +1,43 @@
 fn main() {
-    println!("{}", hello_world());
+    println!("{}", hello("meow", "SPANISH"));
 }
 
 
-fn hello_world() -> String {
-    return "Hello, World!".to_string();
+const ENGLISHHELLOPREFIX: &str = "Hello, ";
+const SPANISHHELLOPREFIX: &str = "Hola, ";
+const FRENCHHELLOPREFIX: &str = "Bonjour, ";
+
+
+fn hello(mut name: &str, language: &str) -> String {
+    if name == "" {
+        name = "World";
+    }
+    let language = language.to_lowercase();
+    let prefix = prefix(&language);
+    return format!("{prefix}{name}!")
 }
+
+
+fn prefix(language: &str) -> &str {
+    match language {
+        "spanish" => return SPANISHHELLOPREFIX,
+        "french" => return FRENCHHELLOPREFIX,
+        _ => return ENGLISHHELLOPREFIX,
+    }
+}
+
+
+/*
+fn prefix(language: &str) -> &str {
+    if language.to_lowercase() == "spanish" {
+        return SPANISHHELLOPREFIX
+    } else if language.to_lowercase() == "french" {
+       return FRENCHHELLOPREFIX
+    } else {
+        return ENGLISHHELLOPREFIX
+    }
+}
+*/
 
 
 #[cfg(test)]
@@ -13,8 +45,38 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_hello_world() {
-        let result = hello_world();
+    fn test_hello_empty() {
+        let result = hello("", "ENGLISH");
         assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn test_hello() {
+        let result = hello("Meowth", "ENGLISH");
+        assert_eq!(result, "Hello, Meowth!");
+    }
+
+    #[test]
+    fn test_hello_spanish_empty() {
+        let result = hello("", "SPANISH");
+        assert_eq!(result, "Hola, World!");
+    }
+
+    #[test]
+    fn test_hello_spanish() {
+        let result = hello("Ada", "SPANISH");
+        assert_eq!(result, "Hola, Ada!");
+    }
+
+    #[test]
+    fn test_hello_french_empty() {
+        let result = hello("", "FRENCH");
+        assert_eq!(result, "Bonjour, World!");
+    }
+
+    #[test]
+    fn test_hello_french() {
+        let result = hello("Kat", "FRENCH");
+        assert_eq!(result, "Bonjour, Kat!");
     }
 }
