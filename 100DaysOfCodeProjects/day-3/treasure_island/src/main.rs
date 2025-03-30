2527ed754cbc2c17472544ccdeae00774a795884
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-03-30 17:30:16 -0500

    Add decision functions.

--- 100DaysOfCodeProjects/day-3/treasure_island/src/main.rs
+++ 100DaysOfCodeProjects/day-3/treasure_island/src/main.rs
@@ -13,6 +13,7 @@ fn decision_input(decision: i32) -> String {
         0 => println!("You are at a crossroad. Where do you want to go? Type 'Left' or 'Right'."),
         1 => println!("You come across a lake. What would you like to do? Type 'Swim' or 'Wait'."),
         2 => println!("Game over. Would you like to play again? 'Yes' or 'No'."),
+        3 => println!("You come across three doors. Which door would you like to go through? 'Red' or 'Yellow' or 'Blue'.");
         _ => return "Meow".to_string()
     }
 
@@ -27,6 +28,7 @@ fn decision_string(decision: String) -> i32 {
     match decision.to_lowercase().as_str() {
         "l\n" | "left\n" => return 1,
         "r\n" | "right\n" => return 2,
+        "red\n" => return 3,
         "y\n" | "yes\n" => return 13,
         "n\n" | "no\n" => 14,
         _ => return 99,
@@ -38,6 +40,7 @@ fn decision_output(decision: i32) -> String {
     match decision {
         1 => return "Went left down the road.".to_string(),
         2 => return "Fell into a hole.".to_string(),
+        3 => return "Burned by fired.".to_string(),
         _ => return "Meow".to_string()
     }
 }
