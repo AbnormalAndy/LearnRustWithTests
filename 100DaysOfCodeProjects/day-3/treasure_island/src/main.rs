08e734f8845244b5456da60aaf76f499bae924e5
AbnormalAndy <97757029+AbnormalAndy@users.noreply.github.com>
2025-03-30 10:46:16 -0500

    Add decision functions.

--- 100DaysOfCodeProjects/day-3/treasure_island/src/main.rs
+++ 100DaysOfCodeProjects/day-3/treasure_island/src/main.rs
@@ -6,34 +6,70 @@ use std::io;
 // Function to evaluate previous function output.
 // - Gives output of number.
 
+
 fn decision_input(decision: i32) -> String {
     let mut answer = String::new();
     match decision {
         0 => println!("You are at a crossroad. Where do you want to go? Type 'Left' or 'Right'."),
         1 => println!("You come across a lake. What would you like to do? Type 'Swim' or 'Wait'."),
+        2 => println!("Game over. Would you like to play again? 'Yes' or 'No'."),
         _ => return "Meow".to_string()
     }
+
+
     io::stdin().read_line(&mut answer).unwrap();
     return answer
 }
 
 
 fn decision_string(decision: String) -> i32 {
+    // Will return the same string if chosen.
     match decision.to_lowercase().as_str() {
         "l\n" | "left\n" => return 1,
+        "r\n" | "right\n" => return 2,
+        "y\n" | "yes\n" => return 13,
+        "n\n" | "no\n" => 14,
         _ => return 99,
+    }
+}
+
+
+fn decision_output(decision: i32) -> String {
+    match decision {
+        1 => return "Went left down the road.".to_string(),
+        2 => return "Fell into a hole.".to_string(),
+        _ => return "Meow".to_string()
     }
 }
 
 
 fn main() {
     let mut decision_number = 0;
-    //let mut decision_string = String::new();
+    let mut game_on = true;
+
 
     println!("Welcome to Treasure Island");
     println!("Your mission is to find the treasure.");
-    decision_number = decision_string(decision_input(decision_number));
-    println!("{}", decision_number);
+
+
+    while game_on == true {
+        println!("{}", decision_number);
+        decision_number = decision_string(decision_input(decision_number));
+        println!("{}", decision_number);
+        println!("{}", decision_output(decision_number));
+        
+
+        // Need win / lose condition.
+        if decision_number == 99 {
+            println!("Invalid input.");
+            game_on = false;
+        } else if decision_number == 14 {
+            println!("Thank you for playing!");
+            game_on = false;
+        } else if decision_number == 13 {
+            main();
+        }
+    }
 }
 
 
